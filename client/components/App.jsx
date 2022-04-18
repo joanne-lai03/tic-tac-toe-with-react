29a95a6c336418debe9e69d53bae8fcc5d59bf52
joanne-lai03 <joanne.lai92@gmail.com>
2022-04-18 21:15:20 +1200

    Set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,37 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient'
+import React from 'react'
+import Game from './Game'
 
-const App = () => {
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-  const [isError, setIsError] = useState(false)
+export default function App () {
 
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-        setIsError(false)
-        return null
-      })
-      .catch((err) => {
-        console.log(err)
-        setIsError(true)
-      })
-  }, [count])
-
-  return (
-    <>
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-    </>
-  )
+ return (
+ <Game />
+ )
 }
 
-export default App
+
