d5b71d6e34d716dc461580d5512de0a328071bec
joanne-lai03 <joanne.lai92@gmail.com>
2022-04-19 18:28:04 +1200

    Set squares

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { calculateWinner } from "../Helpers";
 import Board from "./Board";
 
 export default function Game () {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+  const winner = calculateWinner(board)
+
   function handleClick () {
-    
+    const boardCopy = [...board]
   }
 
   return ( 
-    <Board onClick={handleClick} />
+    <Board squares={board} onClick={handleClick} />
   )
 }
